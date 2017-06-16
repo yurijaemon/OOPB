0aa793fbcfdb689e4286e33fb18c62c32e046052
Yuri Jaeger Monti <yurijm@gmail.com>
2017-06-15 22:40:58 -0300

    - Adicionado atributos

--- src/br/com/duasmaos/main/main.java
+++ src/br/com/duasmaos/main/main.java
@@ -1,12 +1,16 @@
 package br.com.duasmaos.main;
 
+import br.com.duasmaos.modelo.Cachorro;
+import br.com.duasmaos.modelo.Pessoa;
+
 /**
  * Created by yurijm on 15/06/17.
  */
 public class main {
 
     public static void main(String[] args){
-        
+        Pessoa josefina = new Pessoa();
+        Cachorro salomao = new Cachorro();
     }
 
 }
