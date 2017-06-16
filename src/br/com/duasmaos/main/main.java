a437c5e743a2321aa5428f4cc027005902cb69c5
Yuri Jaeger Monti <yurijm@gmail.com>
2017-06-15 22:27:44 -0300

    - Corrigido classe main

--- src/br/com/duasmaos/main/main.java
+++ src/br/com/duasmaos/main/main.java
@@ -3,6 +3,10 @@ package br.com.duasmaos.main;
 /**
  * Created by yurijm on 15/06/17.
  */
-public static void main(String[] args){
+public class main {
+
+    public static void main(String[] args){
+        
+    }
 
 }
