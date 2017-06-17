d25e0b34b98a155cdb63374bd2b76862eb3cc78b
Yuri Jaeger Monti <yurijm@gmail.com>
2017-06-17 14:36:51 -0300

    - Adicionado Classes CachorroQuente e Jogador

--- src/br/com/duasmaos/main/main.java
+++ src/br/com/duasmaos/main/main.java
@@ -2,6 +2,8 @@ package br.com.duasmaos.main;
 
 import br.com.duasmaos.modelo.Cachorro;
 import br.com.duasmaos.modelo.Pessoa;
+import br.com.duasmaos.modelo.CachorroQuente;
+import br.com.duasmaos.modelo.Jogador;
 
 /**
  * Created by yurijm on 15/06/17.
@@ -9,17 +11,21 @@ import br.com.duasmaos.modelo.Pessoa;
 public class main {
 
     public static void main(String[] args){
+
         Pessoa josefina = new Pessoa();
         Cachorro salomao = new Cachorro();
-
         String nome;
+
         int tamanhoNome;
+        int Teste[];
+        Teste = new int[10];
 
         josefina.setNome(null);
         nome = josefina.getNome();
 
         try {
-            tamanhoNome = nome.length();
+            //tamanhoNome = nome.length();
+            Teste[11] = 10;
         }
         catch(NullPointerException e) {
             // Fudeu batima
