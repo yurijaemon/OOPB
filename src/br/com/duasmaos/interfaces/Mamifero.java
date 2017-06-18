cbbf2a6a4cd126a32b9d2f8b581a0b80241ce6d3
Yuri Jaeger Monti <yurijm@gmail.com>
2017-06-17 21:20:33 -0300

    - Adicionado interface de mamiferos

---
+++ src/br/com/duasmaos/interfaces/Mamifero.java
@@ -0,0 +1,15 @@
+package br.com.duasmaos.interfaces;
+
+/**
+ * Created by yurijm on 17/06/17.
+ */
+public interface Mamifero {
+
+    public void mama();
+    public void come();
+    public void bebe();
+    public void fazCoco(int tamanho);
+    public void amamenta();
+    public void berra();
+
+}
