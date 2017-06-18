d176b1d7c400cca26bcfc1895fb3586b0c78a4d2
Yuri Jaeger Monti <yurijm@gmail.com>
2017-06-17 21:03:17 -0300

    - Adicionado Enum de tamanhos

---
+++ src/br/com/duasmaos/br/com/duasmaos/enums/TamanhoCachorro.java
@@ -0,0 +1,8 @@
+package br.com.duasmaos.br.com.duasmaos.enums;
+
+/**
+ * Created by yurijm on 17/06/17.
+ */
+public enum TamanhoCachorro {
+    PEQUINES, PEQUENO, MEDIO, GRANDE, GIGANTE, CRISTIAN, PUTAMERDA, YOURMAMA
+}
